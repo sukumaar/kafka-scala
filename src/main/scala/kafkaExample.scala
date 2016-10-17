2cec94754a778610bc3eaafc8034236bb7d7df05
Sukumaar <sukumaar@gmail.com>
2016-10-17 16:44:28 +0530

    Handled exception for corrupted configuration

--- src/main/scala/kafkaExample.scala
+++ src/main/scala/kafkaExample.scala
@@ -7,15 +7,9 @@ import org.apache.kafka.clients.producer.KafkaProducer
 import com.typesafe.config.ConfigFactory
 import com.typesafe.config.Config
 
-object KafkaScalaProducer {
-  def sendMsg(msg: String, conf: String): Unit = {
-    val kafkaProducerConf = ConfigFactory.load(conf)
-    // you need to provide java.util.Properties to KafkaProducer's constructor 
-    val producer = new KafkaProducer[Integer, String](getProperties(kafkaProducerConf))
-
-    //for (i <- 1 to 10) {
-    //val record = new ProducerRecord[Integer, String](kafkaProducerConf.getString("topicName"), 1, s"ubi test @ [${java.util.Calendar.getInstance().getTime()}] iteration num=$i")
-    val record = new ProducerRecord[Integer, String](kafkaProducerConf.getString("topicName"), 1, s"$msg")
+case class KafkaScalaProducer(producer: KafkaProducer[Integer, String], topicName: String) {
+  def sendMsg(msg: String): Unit = {
+    val record = new ProducerRecord[Integer, String](s"$topicName", 1, s"$msg")
     val metaDataFuture = producer.send(record)
 
     /*val metaInfo = metaDataFuture.get() // blocking!
@@ -24,18 +18,58 @@ object KafkaScalaProducer {
       println(s"Interation = $i $msgLog ")*/
 
     //}
-    producer.close()
+
+  }
+  def closeProducer() = { producer.close() }
+
+}
+
+object KafkaScalaProducer {
+  def apply(confToUse: String) = {
+    val kafkaProducerConf = ConfigFactory.load(confToUse)
+    checkThenGetProperties(kafkaProducerConf) match {
+      case Some(properties) => {
+
+        try {
+          val kfp = new KafkaProducer[Integer, String](properties)
+          new KafkaScalaProducer(kfp, s"${kafkaProducerConf.getString("topicName")}")
+        } catch {
+
+          case ex: org.apache.kafka.common.KafkaException =>
+            {
+
+              ex.getCause match {
+                case configEx: org.apache.kafka.common.config.ConfigException => s"${configEx.getMessage}"
+              }
+            }
+          case t: Throwable => t.printStackTrace(); t.getMessage // any unhandled error will go here
+          //org.apache.kafka.common.config.ConfigException: No resolvable bootstrap urls given in bootstrap.servers
+        }
+
+      }
+      case _ => None
+    }
+
   }
 
-  def getProperties(producerPropertyFile: Config) = {
+  def checkThenGetProperties(producerPropertyFile: Config): Option[java.util.Properties] = {
     //val conf = ConfigFactory.load(producerPropertyFile)
     val props = new java.util.Properties()
-    props.put("bootstrap.servers", producerPropertyFile.getString("bootstrap.servers"))
-    props.put("client.id", producerPropertyFile.getString("client.id"))
-    props.put("key.serializer", producerPropertyFile.getString("key.serializer"))
-    props.put("value.serializer", producerPropertyFile.getString("value.serializer"))
-    //you can add here rest of the properties, for reference have look at kafkaProducer.properties in resources
-    props
+    (producerPropertyFile.hasPath("bootstrap.servers"),
+      producerPropertyFile.hasPath("client.id"),
+      producerPropertyFile.hasPath("key.serializer"),
+      producerPropertyFile.hasPath("value.serializer")) match {
+        case (true, true, true, true) => {
+          props.put("bootstrap.servers", producerPropertyFile.getString("bootstrap.servers"))
+          props.put("client.id", producerPropertyFile.getString("client.id"))
+          props.put("key.serializer", producerPropertyFile.getString("key.serializer"))
+          props.put("value.serializer", producerPropertyFile.getString("value.serializer"))
+          //you can add here rest of the properties, for reference have look at kafkaProducer.properties in resources
+          Option(props)
+        }
+        case (_, _, _, _) => None
+      }
+
   }
 
 }
--- src/main/scala/mainClass.scala
+++ src/main/scala/mainClass.scala
@@ -1,5 +1,20 @@
 
 
-object mainClass extends App{
-  KafkaScalaProducer.sendMsg("bye","kafkaProducer")
+object mainClass extends App {
+  val ff = Option(KafkaScalaProducer("kafkaProducer"))
+
+  ff match {
+    case Some(ff: KafkaScalaProducer) => {
+      for (i <- 0 to 1) {
+        ff.sendMsg(s"Current time is ${java.util.Calendar.getInstance().getTime()}")
+      }
+
+      println("closing producer")
+      ff.closeProducer()
+      println("Done From Running")
+    }
+    case Some(exceptionMsg:String) => println(exceptionMsg)
+    case _ =>println("Unable to get instance of KafkaScalaProducer")
+  }
+
 }
